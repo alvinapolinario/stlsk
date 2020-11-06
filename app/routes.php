82c43358e88b1f5f0cf28884c6d4faf509fb5279
Alvin Apolinario <apolinario.alvin@gmail.com>
2020-11-07 04:04:50 +0800

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,7 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write('Hello Buddy');
         return $response;
     });
 
